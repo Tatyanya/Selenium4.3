b18d24accc7f261f77a92eddb2fc1aabd5932a17
Tatyana Korobitsina <Tatyana.Korobitsina@thomsonreuters.com>
2016-08-08 20:12:16 +0300

    add js

--- src/test/java/com/epam/AppTest.java
+++ src/test/java/com/epam/AppTest.java
@@ -5,15 +5,11 @@ import com.epam.tam.module4.task3.browser.Browser;
 import com.epam.tam.module4.task3.page_object.GoogleHomePage;
 import com.epam.tam.module4.task3.page_object.LipsumHomePage;
 import com.epam.tam.module4.task3.util.SentanceSeparator;
-import mx4j.tools.config.DefaultConfigurationBuilder;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.*;
 import org.openqa.selenium.Point;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.internal.Locatable;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -21,6 +17,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 
 public class AppTest {
@@ -31,6 +30,7 @@ public class AppTest {
     protected static RemoteWebDriver driver = Browser.getInstance().getDriver();
     LipsumHomePage lipsumHomePage = new LipsumHomePage();
     GoogleHomePage sf = new GoogleHomePage();
+    JavascriptExecutor js = (JavascriptExecutor) driver;
 
     @BeforeClass(description = "Maximize window")
     public void maximizeBrowser() {
@@ -39,7 +39,7 @@ public class AppTest {
 
 
     @Test()
-    public void goToSite() throws InterruptedException, AWTException {
+    public void selectionActions() throws InterruptedException {
 
         //  LOG.info("Open start page");
         driver.navigate().to(START_URL);
@@ -93,11 +93,43 @@ public class AppTest {
         Assert.assertTrue(sf.results.isDisplayed(), "error");
     }
 
-    @Test(dependsOnMethods = "goToSite", description = "LogIn to PL")
-    public void failedLogIn() {
+    @Test(description = "JS")
+    public void selectionJS() throws InterruptedException {
+
+        driver.navigate().to(START_URL);
+        lipsumHomePage.openGeneratedPage();
+
+        WebElement paragraph = driver.findElement(By.cssSelector("#lipsum p:nth-child(2)"));
+        String text = copyTextFrom(paragraph);
+        driver.navigate().to("google.com");
+        runSearchFor(text);
+        verifyResult();
 
     }
 
+
+    public String copyTextFrom(WebElement element) {
+        return js.executeScript(
+                "var text =(arguments[0].innerHTML.toString().split('.')[2]);\n" +
+                        "var selection = window.getSelection();\n" +
+                        "var range = document.createRange();\n" +
+                        "  range.selectNodeContents(arguments[0]);\n" +
+                        " selection.removeAllRanges();\n" +
+                        " selection.addRange(range);\n" +
+                        " return window.getSelection().toString();", element).toString();
+    }
+
+    public void runSearchFor(String text) {
+        js.executeScript("var input = document.getElementById(\"lst-ib\"); \n" +
+                "input.value = arguments[0];\n" +
+                "document.forms[0].submit();", text);
+    }
+
+    public void verifyResult() {
+        //js.executeScript("return document.getElementById('resultStats');");
+        assertNotNull(js.executeScript("return document.getElementById('resultStats');"),"Null results");
+
+    }
 
     @AfterClass(description = "Stop Browser")
     public void stopBrowser() {
